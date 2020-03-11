ab783961f7c0647133f42ce502dbf12014b60cbe
Kamil Krysiak <kamkry99@gmail.com>
2020-03-11 13:35:19 +0100

    Connect with zeit/now

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import MapBox, { Marker } from 'react-map-gl';
+import React, { useState } from 'react';
+import MapBox from 'react-map-gl';
 import Vehicle from '../Vehicle';
 
 const Map: React.FC = () => {
