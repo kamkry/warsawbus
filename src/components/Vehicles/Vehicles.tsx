c15ae5e56c2574aedc600b7b2c29cf9c97f098d7
Kamil Krysiak <kamkry99@gmail.com>
2020-03-11 23:36:01 +0100

    Add icon rotation when changing position

--- src/components/Vehicles/Vehicles.tsx
+++ src/components/Vehicles/Vehicles.tsx
@@ -1,21 +1,65 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import axios from 'axios';
 import Vehicle from '../Vehicle';
 import { VehicleProps } from '../Vehicle/Vehicle';
 
+function positionNotChanged(vehicle: VehicleProps, lastVehicle: VehicleProps) {
+  return (
+    vehicle.longitude !== lastVehicle.longitude &&
+    vehicle.latitude !== lastVehicle.latitude
+  );
+}
+function hasTheSameId(vehicle: VehicleProps) {
+  return (v: VehicleProps) => vehicle.number === v.number;
+}
+
+function calculateRotation(vehicle: VehicleProps, lastVehicle: VehicleProps) {
+  return (
+    (Math.atan2(
+      vehicle.longitude - lastVehicle.longitude,
+      vehicle.latitude - lastVehicle.latitude
+    ) *
+      180) /
+    Math.PI
+  );
+}
+
+function updateProperties(lastData: VehicleProps[]) {
+  return axios('/api/locations?type=1&line=109').then(res => {
+    return res.data.map((vehicle: VehicleProps) => {
+      const lastVehicle = lastData.filter(hasTheSameId(vehicle))[0];
+      if (!lastVehicle) return vehicle;
+      const newVehicle = { ...vehicle };
+      if (positionNotChanged(vehicle, lastVehicle)) {
+        newVehicle.rotate = calculateRotation(vehicle, lastVehicle);
+      } else {
+        newVehicle.rotate = lastVehicle.rotate;
+      }
+      return newVehicle;
+    });
+  });
+}
+
 const Vehicles: React.FC = () => {
-  const [data, setData] = useState([]);
-  const getData = () => {
-    fetch('/api/locations?type=1&line=109')
-      .then(res => res.json())
-      .then(setData);
-  };
+  const [data, setData] = useState([] as VehicleProps[]);
+  const [lastData, setLastData] = useState([] as VehicleProps[]);
+
+  const update = useCallback(() => {
+    updateProperties(lastData).then(newData => {
+      setData(old => {
+        setLastData(old);
+        return newData;
+      });
+    });
+  }, [lastData]);
 
   useEffect(() => {
-    getData();
-    const interval = setInterval(getData, 5_000);
+    update();
+    const interval = setInterval(update, 1_000);
     return () => clearInterval(interval);
-  }, []);
+  }, [data, lastData, update]);
+
   if (!data) {
     return null;
   }
@@ -28,7 +72,7 @@ const Vehicles: React.FC = () => {
             line="109"
             latitude={vehicle.latitude}
             longitude={vehicle.longitude}
-            rotate={0}
+            rotate={vehicle.rotate}
           />
         );
       })}
