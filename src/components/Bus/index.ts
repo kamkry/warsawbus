7b2111b5c83d4a99dc1015ca3929499c51362eb6
Kamil Krysiak <kamkry99@gmail.com>
2020-03-14 21:57:37 +0100

    Update relative imports to absolute

--- src/components/Bus/index.ts
+++ src/components/Bus/index.ts
@@ -1,1 +1,10 @@
 export { default } from './Bus';
+
+export interface BusProps {
+  number?: string;
+  line: string;
+  time?: string;
+  latitude: number;
+  longitude: number;
+  rotate: number;
+}
