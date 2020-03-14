3e50d9ce47375ed780dc3562453eec7fa9941cee
Kamil Krysiak <kamkry99@gmail.com>
2020-03-14 22:48:34 +0100

    Change bar font size

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -15,14 +15,14 @@ const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 4rem;
-  height: 4rem;
+  width: 3rem;
+  height: 3rem;
   border: none;
   font: inherit;
   cursor: pointer;
 `;
 const InputWrapper = styled.div<BarProps>`
-  height: 4rem;
+  height: 3rem;
   background-color: white;
   display: flex;
   align-items: center;
@@ -32,16 +32,16 @@ const SearchInput = styled.input<BarProps>`
   width: ${({ open }) => (open ? '16rem' : '0')};
   visibility: ${({ open }) => (open ? 'visible' : 'hidden')};
   transition: width 0.2s ease-out;
-  font-size: 1.3rem;
+  font-size: 1.2rem;
   border: 0;
 `;
 
 const StyledSearchIcon = styled(SearchIcon)`
-  transform: scale(1.3);
+  transform: scale(1.1);
 `;
 
 const SearchBar = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   return (
     <StyledBar open={open}>
       <Button onClick={() => setOpen(!open)}>
